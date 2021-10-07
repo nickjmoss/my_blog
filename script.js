8fcee8c25065c8488838b3762e693b512752c32a
Nick J Moss <78372873+nickjmoss@users.noreply.github.com>
2021-10-07 10:00:38 -0600

    good updates

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 window.onload = function() {    
     let modal = document.getElementById("modal");
     let button = document.getElementById("button");
-    let close = document.getElementById("closer");
+    let close = document.getElementById("close");
+    let body = document.querySelector("body");
 
     button.onclick = function() {
-        modal.style.display = 'block';
+        modal.style.display = "block";
+        body.style.overflow = "hidden";
     }
 
     close.onclick = function () {
-        modal.style.display = 'none';
+        modal.style.display = "none";
+        body.style.overflow = "auto";
     }
 }
