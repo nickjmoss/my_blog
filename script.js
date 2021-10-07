b1f814429fc8b6c5ffbb3d3fe79d0d0d2232e6fa
Nick J Moss <78372873+nickjmoss@users.noreply.github.com>
2021-10-07 15:30:07 -0600

    learned JS things

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ window.onload = function() {
     let button = document.getElementById("button");
     let close = document.getElementById("close");
     let body = document.querySelector("body");
+    let window = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
+    let targetWidth = 768;
 
     button.onclick = function() {
         modal.style.display = "block";
@@ -12,5 +14,13 @@ window.onload = function() {
     close.onclick = function () {
         modal.style.display = "none";
         body.style.overflow = "auto";
+    }
+
+    if (window <= targetWidth) {
+        let svgList = document.getElementsByTagName('svg');
+        for (let svg of svgList) {
+            svg.setAttribute('width', '36');
+            svg.setAttribute('height', '36');
+        }
     }
 }
