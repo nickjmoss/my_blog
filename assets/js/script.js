7a4d3e17ff4b45d4998990b4d17ae543c3cce786
Nick J Moss <78372873+nickjmoss@users.noreply.github.com>
2021-10-15 11:02:49 -0600

    projects page + github API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {    
+document.addEventListener("DOMContentLoaded", function() {
     let modal = document.getElementById("modal");
     let button = document.getElementById("menu-icon");
     let body = document.querySelector("body");
-
 
     function toggleMenuIcon() {
         button.classList.toggle("active");
@@ -11,4 +10,46 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     
     button.addEventListener("click", toggleMenuIcon);
+
+    // Adds most recent GitHub Repos to Featured Grid on Projects Page
+    function getGithubRepos() {
+        let githubApi = "https://api.github.com/users/nickjmoss/repos"
+        const fetchPromise = fetch(githubApi);
+        fetchPromise.then(response => {
+            return response.json();
+        }).then(data => {
+            let projectId = 1;
+            for (let repoData of data) {
+                let repoName = repoData.name;
+                let repoLang = repoData.language;
+                let repoDesc = repoData.description;
+                let repoLink = `https://github.com/nickjmoss/${repoName}`;
+
+                let finalName = ''
+
+                if (repoName.includes('_')) {
+                    let nameList = repoName.split('_');
+                    for (let i = 0; i < nameList.length; i++) {
+                        let myWord = nameList[i].charAt(0).toUpperCase() + nameList[i].slice(1);
+                        nameList[i] = myWord;
+                    }
+                    finalName = nameList.join(' ');
+                }
+                else {
+                    finalName = repoName.charAt(0).toUpperCase() + repoName.slice(1);
+                }
+                
+                document.getElementById(`project-item-${projectId}`).children[0].children[0].innerHTML=finalName;
+                document.getElementById(`project-item-${projectId}`).children[0].children[1].innerHTML=repoLang;
+                document.getElementById(`project-item-${projectId}`).children[0].children[2].innerHTML=repoDesc;
+                document.getElementById(`project-item-${projectId}`).children[1].children[0].href=repoLink;
+
+                projectId++;
+                
+                
+            }    
+        })
+        
+    }
+    getGithubRepos();
 })
